a82aaeedf60b555ae9f99f5713645b2ef1b36cf5
ÖMER FARUK ERKUL <EXT02D17554@turkcell.entp.tgc>
2019-08-06 08:39:24 +0300

    java8 install completed

--- calistir.ps1
+++ calistir.ps1
@@ -60,17 +60,12 @@ $java8Url = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=239858_2
 $output = "$filePath\jre-8u221-windows-x64.exe"
 (New-Object System.Net.WebClient).DownloadFile($java8Url, $output)
 #Java 8 kurulur.
-Start-Job -Name InstallJava -ScriptBlock {
-    & Start-Process $output -Args '/s INSTALL_SILENT=1 STATIC=0 AUTO_UPDATE=0 WEB_JAVA=1 WEB_JAVA_SECURITY_LEVEL=H WEB_ANALYTICS=0 EULA=0 REBOOT=0 NOSTARTMENU=0 SPONSORS=0 /L "$env:TEMP\jre-8u45-windows-x64.log"'
-    Write-Output $LASTEXITCODE
-}
-Get-Job -Name InstallJava | Wait-Job | Receive-Job
-
+Start-Process $output -Args '/s INSTALL_SILENT=1 STATIC=0 AUTO_UPDATE=0 WEB_JAVA=1 WEB_JAVA_SECURITY_LEVEL=H WEB_ANALYTICS=0 EULA=0 REBOOT=0 NOSTARTMENU=0 SPONSORS=0 /L "$env:TEMP\jre-8u45-windows-x64.log"'
 
 Remove-Item $output -Force
 
 # Get current config Jenkins
-###curl -X GET http://developer:developer@localhost:8080/job/test/config.xml -o mylocalconfig.xml
+###curl -X GET http://MAYA10:113aea2aae91d13fbed3fba5f9d15eab0c@localhost:8080/job/test/config.xml -o mylocalconfig.xml
 
 # Post updated config Jenkins
 ###curl -X POST http://developer:developer@localhost:8080/job/test/config.xml --data-binary "@mymodifiedlocalconfig.xml"
