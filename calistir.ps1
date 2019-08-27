029c0e43f97b8259d85e00d54627dfef835df3ae
ÖMER FARUK ERKUL <EXT02D17554@turkcell.entp.tgc>
2019-08-27 13:20:33 +0300

    process bar added

--- calistir.ps1
+++ calistir.ps1
@@ -64,14 +64,12 @@ function Request-JenkinsXml([string] $userLdap,[string] $hostname) {
         ## reset headers for client re-use if you're processing many jobs
         ## (or .Dispose() the client if you're done)
         $webClient.Headers.Remove([System.Net.HttpRequestHeader]::ContentType)
-
     } #>
 
     
     $config
 }
 function Write-BatFile([string] $filePath) {
-    
     ##Bat dosyasi yazdir.
     $text = 'SETLOCAL
     SET JAVA_TOOL_OPTIONS=
@@ -100,7 +98,8 @@ function Download-ChromeDriver([string] $filePath) {
     $WebResponse = $WebResponse.Content
     $DownloadUrl = "https://chromedriver.storage.googleapis.com/$WebResponse/chromedriver_win32.zip"
     $output = "$filePath\chromeDriver.zip"
-    (New-Object System.Net.WebClient).DownloadFile($DownloadUrl, $output)
+    Get-FileFromURL -URL $DownloadUrl -Filename $output
+    #(New-Object System.Net.WebClient).DownloadFile($DownloadUrl, $output)
     $hasDownloaded = $?
     if ($PSVersionTable.PSVersion.ToString().split(".", 2).get(0) -ge 5) { Expand-Archive -path $output -destinationpath $filePath -Force }
         
@@ -109,18 +108,19 @@ function Download-ChromeDriver([string] $filePath) {
     $hasDownloaded
 }
 function Download-SeleniumJar([string] $filePath) {
-    
     #Selenium jar dosyasi indirilir.
     $SeleniumJarUrl = "https://selenium-release.storage.googleapis.com/3.12/selenium-server-standalone-3.12.0.jar"
     $output = "$filePath\selenium-server-standalone-3.12.0.jar"
-    (New-Object System.Net.WebClient).DownloadFile($SeleniumJarUrl, $output)
+    Get-FileFromURL -URL $SeleniumJarUrl -Filename $output
+    #(New-Object System.Net.WebClient).DownloadFile($SeleniumJarUrl, $output)
     $?
 }
 function Setup-Java([string] $filePath) {
     #Java 8 indirilir ve kurulur.
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $URL = (Invoke-WebRequest -UseBasicParsing https://www.java.com/en/download/manual.jsp).Content | % { [regex]::matches($_, '(?:<a title="Download Java software for Windows Online" href=")(.*)(?:">)').Groups[1].Value }
-    Invoke-WebRequest -UseBasicParsing -OutFile "$filePath\jre8.exe" $URL
+    $URL = (Invoke-WebRequest -UseBasicParsing https://www.java.com/en/download/manual.jsp).Content | % { [regex]::matches($_, '(?:<a title="Download Java software for Windows [(]64-bit[)]" href=")(.*)(?:">)').Groups[1].Value }
+    Get-FileFromURL -URL $URL -Filename "$filePath\jre8.exe"
+    #Invoke-WebRequest -UseBasicParsing -OutFile "$filePath\jre8.exe" $URL
     Start-Process "$filePath\jre8.exe" '/s REBOOT=0 SPONSORS=0 AUTO_UPDATE=0' -Wait
     $?
 }
@@ -130,6 +130,77 @@ function Get-TimeStamp {
     
 }
 
+function Get-FileFromURL {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [System.Uri]$URL,
+        [Parameter(Mandatory, Position = 1)]
+        [string]$Filename
+    )
+
+    process {
+        try {
+            $request = [System.Net.HttpWebRequest]::Create($URL)
+            $request.set_Timeout(5000) # 5 second timeout
+            $response = $request.GetResponse()
+            $total_bytes = $response.ContentLength
+            $response_stream = $response.GetResponseStream()
+
+            try {
+                # 256KB works better on my machine for 1GB and 10GB files
+                # See https://www.microsoft.com/en-us/research/wp-content/uploads/2004/12/tr-2004-136.pdf
+                # Cf. https://stackoverflow.com/a/3034155/10504393
+                $buffer = New-Object -TypeName byte[] -ArgumentList 256KB
+                $target_stream = [System.IO.File]::Create($Filename)
+
+                $timer = New-Object -TypeName timers.timer
+                $timer.Interval = 500 # Update progress every second
+                $timer_event = Register-ObjectEvent -InputObject $timer -EventName Elapsed -Action {
+                    $Global:update_progress = $true
+                }
+                $timer.Start()
+
+                do {
+                    $count = $response_stream.Read($buffer, 0, $buffer.length)
+                    $target_stream.Write($buffer, 0, $count)
+                    $downloaded_bytes = $downloaded_bytes + $count
+
+                    if ($Global:update_progress) {
+                        $percent = $downloaded_bytes / $total_bytes
+                        $status = @{
+                            completed  = "{0,6:p2} Completed" -f $percent
+                            downloaded = "{0:n0} MB of {1:n0} MB" -f ($downloaded_bytes / 1MB), ($total_bytes / 1MB)
+                            speed      = "{0,7:n0} KB/s" -f (($downloaded_bytes - $prev_downloaded_bytes) / 1KB)
+                            eta        = "eta {0:hh\:mm\:ss}" -f (New-TimeSpan -Seconds (($total_bytes - $downloaded_bytes) / ($downloaded_bytes - $prev_downloaded_bytes)))
+                        }
+                        $progress_args = @{
+                            Activity        = "Downloading $URL"
+                            Status          = "$($status.completed) ($($status.downloaded)) $($status.speed) $($status.eta)"
+                            PercentComplete = $percent * 100
+                        }
+                        Write-Progress @progress_args
+
+                        $prev_downloaded_bytes = $downloaded_bytes
+                        $Global:update_progress = $false
+                    }
+                } while ($count -gt 0)
+            }
+            finally {
+                if ($timer) { $timer.Stop() }
+                if ($timer_event) { Unregister-Event -SubscriptionId $timer_event.Id }
+                if ($target_stream) { $target_stream.Dispose() }
+                # If file exists and $count is not zero or $null, than script was interrupted by user
+                if ((Test-Path $Filename) -and $count) { Remove-Item -Path $Filename }
+            }
+        }
+        finally {
+            if ($response) { $response.Dispose() }
+            if ($response_stream) { $response_stream.Dispose() }
+        }
+    }
+}
+
 $filePath = Set-FolderSavePath       #otomasyon klasörü olusturur
 Write-Host "$(Get-TimeStamp) Masaustune otomasyon klasoru olusturuldu..."
 
@@ -137,25 +208,25 @@ Write-Host "$(Get-TimeStamp) Kullanici Bilgileri aliniyor..."
 $userData = Get-UserLdapAndHostname  #Array[0] = LDAP , Array[1] = hostname
 $userLdap = $userData[0]
 $userHostName = $userData[1]
-$jobRequest = "EXT0248898"  #$userLdap`_QaSelenium
+$jobRequest = $userLdap  #$userLdap`_QaSelenium
 
 $jobResponse = Request-JenkinsXml $jobRequest $userHostName
 Write-Host "$(Get-TimeStamp) Jenkins bilgileri ayarlandi..."
 
-$hasDownloaded = Download-ChromeDriver($filePath)
+$hasDownloaded = Download-ChromeDriver $filePath
 Write-Host "$(Get-TimeStamp) Latest chrome driver indirildi..."
 
-$batText = Write-BatFile($filePath)
+$batText = Write-BatFile $filePath
 Write-Host "$(Get-TimeStamp) Bat dosyasi yazildi..."
 
 #Java 8 yüklü değilse yükle.
 if (!((Get-Command java | Select-Object -ExpandProperty Version).tostring() -match "^8.0")) {
-    $setupJava = Setup-Java($filePath)
+    $setupJava = Setup-Java $filePath
     Write-Host "$(Get-TimeStamp) Java versiyon 8 basariyla yuklendi..."
 }
 
 Write-Host "$(Get-TimeStamp) Selenium jar indiriliyor lutfen bekleyin..."
-$downloadSeleniumJar = Download-SeleniumJar($filePath)
+$downloadSeleniumJar = Download-SeleniumJar $filePath
 if($downloadSeleniumJar){
 Write-Host "$(Get-TimeStamp) Selenium jar indirildi..."
 }else{
